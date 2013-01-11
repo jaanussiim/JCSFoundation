1152a3b612a3a9e22d4ad665cc070f496dcba675
JaanusSiim <jaanus@jaanussiim.com>
2013-01-11 20:36:41 +0200

    Initial files

---
+++ JCSFoundation/JCSFoundation.h
@@ -0,0 +1,13 @@
+//
+//  JCSFoundation.h
+//  JCSFoundation
+//
+//  Created by Jaanus Siim on 1/11/13.
+//  Copyright (c) 2013 JaanusSiim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCSFoundation : NSObject
+
+@end
---
+++ JCSFoundation/JCSFoundation.m
@@ -0,0 +1,13 @@
+//
+//  JCSFoundation.m
+//  JCSFoundation
+//
+//  Created by Jaanus Siim on 1/11/13.
+//  Copyright (c) 2013 JaanusSiim. All rights reserved.
+//
+
+#import "JCSFoundation.h"
+
+@implementation JCSFoundation
+
+@end
